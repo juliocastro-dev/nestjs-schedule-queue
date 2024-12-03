3261cecbe2dbfcda2043cae5b561636f53743f70
juliocastro-dev <juliocastro.developer@gmail.com>
2024-12-03 15:19:02 -0300

    Add os tweets a fila sempre que encontrar mais de 10 novos tweets

--- src/tweets/tweets-count/tweets-count.service.ts
+++ src/tweets/tweets-count/tweets-count.service.ts
@@ -4,6 +4,8 @@ import { Tweet } from '../entities/tweet.entity';
 import { Interval } from '@nestjs/schedule';
 import { Cache } from 'cache-manager';
 import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Queue } from 'bull';
+import { InjectQueue } from '@nestjs/bull';
 
 @Injectable()
 export class TweetsCountService {
@@ -13,7 +15,9 @@ export class TweetsCountService {
         private tweetModel: typeof Tweet,
         @Inject(CACHE_MANAGER)
         private cacheManager: Cache,
-      ) {}
+        @InjectQueue('emails')
+        private emailsQueue: Queue
+    ) {}
 
     @Interval(5000)
     async countTweets() {
@@ -36,6 +40,7 @@ export class TweetsCountService {
         if (tweets.length === this.limit) {
             this.cacheManager.set('tweet-offset', offset + this.limit, 0);
             console.log(`achou mais ${this.limit} tweets`);
+            this.emailsQueue.add({tweets: tweets.map((t) => t.toJSON())});
         }
     }
 
